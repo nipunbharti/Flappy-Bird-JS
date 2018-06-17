15a9c8f66920bbb890d60ad80c2dfc274dacd487
Nipun Bharti <ced16i026@iiitdm.ac.in>
2018-06-18 01:13:00 +0530

    Added: Pipes and score

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 var bird;
+var pipes = [];
 function setup(){
 	createCanvas(400, 600);
 	bird = new Bird();
+	pipes.push(new Pipe()); 
 }
 
 function draw(){
 	background(0,255,182);
+
+	for(var i=0;i<pipes.length;i++){
+		pipes[i].show();
+		pipes[i].update();
+		if(pipes[i].off()){
+			pipes.slice(i,1);
+		}
+		if(pipes[i].hits(bird)){
+			location.reload();
+			alert(pipes.length-1);
+		}
+	}
+
 	bird.show();
 	bird.update();
+
+	if(frameCount%40 == 0){
+		pipes.push(new Pipe()); 
+	}
 }
 
 function keyPressed(){
